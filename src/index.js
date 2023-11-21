fede2b2305e03dbafb047d85f298c9a80193b9fd
pedrolucena0523@gmail.com <44994733+pedrolucena05@users.noreply.github.com>
2023-11-20 21:46:04 -0300

    first commit

--- src/index.js
+++ src/index.js
@@ -3,11 +3,20 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import Footer from './components/footer';
+import App2 from './components/app';
+import Main from './components/main';
+import Header from './components/header';
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
+    <App2 />
+    <Header />
+    <Main />
+    <Footer />
   </React.StrictMode>
 );
 
